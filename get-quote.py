93f927ca7cb6b3124d1b1ebe4de2d85765ef815c
toojajam <guiumdongi@gmail.com>
2020-09-04 11:14:23 +0900

    This is the end.

--- get-quote.py
+++ get-quote.py
@@ -10,8 +10,14 @@ def primary():
 
     last = 13
     rnd = random.randint(0, last)
+    print(quotes[rnd], end="")
 
+    rnd = random.randint(0, last)
     print(quotes[rnd])
+
+    f = open("quotes.txt", "a", encoding="utf8")
+    f.write("Adding more lines to an existing text file." + "\n")
+    f.close()
 
 
 if __name__ == "__main__":
